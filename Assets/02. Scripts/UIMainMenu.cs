770815d0ee5ff3a320153d89eb44f11a545c95d5
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-03-21 10:14:11 +0900

    Feat: "각종 스크립트 작성 완료"

--- Assets/02. Scripts/UIMainMenu.cs
+++ Assets/02. Scripts/UIMainMenu.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class UIMainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //메인 메뉴를 활성화할 때 사용하는 메서드
+    private void OpenMainMenu()
     {
-        
+        UIManager.Instance.Menu.gameObject.SetActive(true);
+    }
+
+
+    //스테이터스를 활성화할 때 사용하는 메서드
+    private void OpenStatus()
+    {
+        UIManager.Instance.Status.gameObject.SetActive(true);
+    }
+
+
+    //인벤토리를 활성화할 때 사용하는 메서드
+    private void OpenInventory()
+    {
+        UIManager.Instance.Inventory.gameObject.SetActive(true);
     }
 }
--- Assets/02. Scripts/UIManager.cs
+++ Assets/02. Scripts/UIManager.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
+    [SerializeField] private UIMainMenu menu;
+    [SerializeField] private UIInventory inventory;
+    [SerializeField] private UIStatus status;
 
+    public UIMainMenu Menu { get => menu; }
+    public UIInventory Inventory { get => inventory; }
+    public UIStatus Status { get => status; }
 }
