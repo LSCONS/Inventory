d3610e26b0d6c55a3b3671e1eaba6e0e5fd7f486
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-03-21 12:54:50 +0900

    Feat: "인벤토리 자동 생성 완료"

--- Assets/02. Scripts/UIInventory.cs
+++ Assets/02. Scripts/UIInventory.cs
@@ -5,11 +5,20 @@ using UnityEngine.UI;
 
 public class UIInventory : MonoBehaviour
 {
-    [SerializeField]private Button button_Back;
+    [SerializeField] private Button button_Back;
+    [SerializeField] private GameObject uiSlot;
+    [SerializeField] private Transform uiSlot_Content;
+
+    private int count_Inventory = 24;
 
     private void Start()
     {
         button_Back.onClick.AddListener(ReturnMainMenu);
+        for(int i = 0; i < count_Inventory; i++)
+        {
+            GameObject temp = Instantiate(uiSlot, uiSlot_Content);
+            temp.GetComponent<UISlot>().slot_Count = i;
+        }
     }
 
 
---
+++ Assets/02. Scripts/UISlot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISlot : MonoBehaviour
+{
+    public int slot_Count = -1;
+    private void SetItem()
+    {
+
+    }
+
+
+    private void RefreshUI()
+    {
+
+    }
+}
